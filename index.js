6e15fd373ca664df57fa7264c2eddbc783e1233e
anturcanu <andrei.turcanu@endava.com>
2021-03-23 09:59:17 +0200

    first 10 challenges

---
+++ index.js
@@ -0,0 +1,19 @@
+// 1. Write a function to check whether a given integer is within 20 of 100 or 400.
+
+// 2. Write a function to check from two given integers, whether one is positive and another one is negative.
+
+// 3. Write a function to remove a character at the specified position of a given string and return the new string.
+
+// 4. Write a function to create a new string from a given string changing the position of first and last characters. The string length must be greater than or equal to 1.
+
+// 5. Write a function to create a new string from a given string with the first character of the given string added at the back.
+
+// 6. Write a function to check whether a given positive number is a multiple of 3 or a multiple of 7.
+
+// 7. Write a function to create a new string from a given string taking the last 3 characters and added at both the front and back. The string length must be 3 or more.
+
+// 8. Write a function to check whether a string "Script" presents at 5th (index 4) position in a given string, if "Script" presents in the string return the string without "Script" otherwise return the original one.
+
+// 9. Write a function to find the largest of three given integers.
+
+// 10. Write a function to find a value which is nearest to 100 from two different given integer values.
